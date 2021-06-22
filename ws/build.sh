a9f1c64324a2eaf92793d72ff99d97556d50e42a
Fred stanley <fred@witesand.io>
2021-06-22 09:44:43 -0700

    osm scale

--- ws/build.sh
+++ ws/build.sh
@@ -2,7 +2,7 @@
 
 #ws docker registry push
 export CTR_REGISTRY=docker.dev.ws:5000
-export CTR_TAG=osmlatest44
+export CTR_TAG=osmlatest44-scale
 make docker-push-osm-controller
 make docker-push-init
 
