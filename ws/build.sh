7f78ec987f72a48f0f40865224bc6fa9dde3cebc
Fred stanley <fred@witesand.io>
2021-07-02 13:41:22 -0700

    add scripts

---
+++ ws/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#ws docker registry push
+export CTR_REGISTRY=docker.dev.ws:5000
+export CTR_TAG=osmlatest46
+make docker-push-osm-controller
+make docker-push-init
+
+#aws docker push
+export CTR_REGISTRY=978944737929.dkr.ecr.us-west-2.amazonaws.com
+make docker-push-osm-controller
+make docker-push-init
